30668eb20d944475028e38a1ca04559d8ab2f4b5
dsdasadasdw <68695976+dsdasadasdw@users.noreply.github.com>
2023-07-22 16:42:43 +0300

    Update iuy.py

--- iuy.py
+++ iuy.py
@@ -5,7 +5,8 @@ from github import Github
 files = ['iuy.py']
 branch = "main"
 repo = 'dsdasadasdw/111'
-token = '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'
+token = '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'
+upload_files_to_github(files, repo, token, branch)
 
 g = Github("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")
 user = g.get_user("dsdasadasdw")
@@ -13,7 +14,7 @@ repo1 = user.get_repo("111")
 contents = repo1.get_contents("iuy.py")
 repo1.delete_file(contents.path, "ree", contents.sha, branch="main")
 
-upload_files_to_github(files, repo, token, branch)
+
 
 
 
