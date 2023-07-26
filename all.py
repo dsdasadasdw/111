d88f2107fd3d12aa4edc5da935cd883758e02a7c
dsdasadasdw <68695976+dsdasadasdw@users.noreply.github.com>
2023-07-27 02:26:34 +0300

    Update all.py

--- all.py
+++ all.py
@@ -20,14 +20,25 @@ app = "ESign.ipa"
 plist = 'ESign.plist'
 plist1 = 'Lrd.plist'
 plist2 = 'Dark.plist'
+eplist = 'ESign1.plist'
+eplist1 = 'Lrd1.plist'
+eplist2 = 'Dark1.plist'
+epliste = 'ESign2.plist'
+eplistl = 'Lrd2.plist'
+eplistd = 'Dark2.plist'
+
+
+
 ipa = 'ESign.ipa'
+
+
 s = random.randint(1, 99999999999999999999999)
-out = f"{s}.ipa"
+out = "1.ipa"
 osi = f"./zsign -k {p12} -p {pass1} -m {prov} -b '{id}' -o {out} -z 9 {app}"
 r = random.randint(1, 99999999999999999999999)
 d = random.randint(1, 99999999999999999999999)
-out1 = f"{r}.ipa"
-out2 = f"{d}.ipa"
+out1 = "2.ipa"
+out2 = "3.ipa"
 
 LRD = "Lrd.ipa"
 DARK = "Dark.ipa"
@@ -35,4 +46,10 @@ DARK = "Dark.ipa"
 LRDosi = f"./zsign -k {p12} -p {pass1} -m {prov} -b '{id1}' -o {out1} -z 9 {LRD}"
 DARKosi = f"./zsign -k {p12} -p {pass1} -m {prov} -b '{id2}' -o {out2} -z 9 {DARK}"
 
+s1 = "s3cmd put 1.ipa s3://242be58c-a70cd3c6-e9a8-49a5-9ac5-c5fb1c3718bc"
+s2 = "s3cmd put 2.ipa s3://242be58c-a70cd3c6-e9a8-49a5-9ac5-c5fb1c3718bc"
+s3 = "s3cmd put 3.ipa s3://242be58c-a70cd3c6-e9a8-49a5-9ac5-c5fb1c3718bc"
 
+deletes1 = "s3cmd del s3://242be58c-a70cd3c6-e9a8-49a5-9ac5-c5fb1c3718bc/1.ipa"
+deletes2 = "s3cmd del s3://242be58c-a70cd3c6-e9a8-49a5-9ac5-c5fb1c3718bc/2.ipa"
+deletes3 = "s3cmd del s3://242be58c-a70cd3c6-e9a8-49a5-9ac5-c5fb1c3718bc/3.ipa"
